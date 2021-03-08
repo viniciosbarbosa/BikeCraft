3edb94c0a6052787f5abdfa17942d73f31025260
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2021-03-08 14:21:25 -0300

    menu ,e prod

--- frontend/Home/Home.js
+++ frontend/Home/Home.js
@@ -1,1 +1,7 @@
-alert("Works")
+
+function ActiveMenu (){
+
+   window.document.getElementById("listmenu").style.display="inline-block"
+   window.document.getElementById("listmenu").style.transition="0.5s"
+    
+}
