bc0e96de35c92dffbf1ca6011350916cbf853e6a
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2021-03-11 01:28:28 -0300

    produtos finalizado

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,27 @@
-$(document).ready(function () {
-    $('telefone').mask('(00) 00000-0000')
-  })
+
+function sumirTxt() {
+    window.document.getElementById("msg").style.color="#fec63e";
+    window.document.getElementById("msg2").style.color="transparent";
+    
+    
+};
+
+function voltartxt(){
+    window.document.getElementById("msg2").style.color="#fec63e";
+    window.document.getElementById("msg").style.color="transparent";
+}
+
+
+function teste(){
+    Swal.fire({
+        
+        icon:'success',
+        title: 'Tranks',
+        background: '#000',
+        confirmButtonColor:'#fec63e',
+        
+    })
+}
+
+
+
